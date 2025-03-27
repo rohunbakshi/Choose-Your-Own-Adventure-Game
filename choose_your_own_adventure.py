2e6f69d2914cd2075397c0163f6136d1cb4b9fdc
rohunbakshi <rohun.bakshi@gmail.com>
2025-03-27 18:31:10 -0500

    bug fixes

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -3,10 +3,10 @@ print("Welcome to this adventure", answer, "At any point, type q to quit. ")
 
 while True:
     answer = input(
-        "You come to a river with a bridge over it. Would you like to swim accross the river or walk across the bridge? ")
+        "You come to a river with a bridge over it. Would you like to swim accross the river or walk across the bridge? swim/walk ")
     if answer.lower() == "swim":
         answer = input(
-            "The river is flowing very fast and you get swept downstream. You start floating towards a waterfall. You see a branch you can grab ahold of and a rock you can sit on. Would you like to try and grab the branch or try and climb the rock? ")
+            "The river is flowing very fast and you get swept downstream. You start floating towards a waterfall. You see a branch you can grab ahold of and a rock you can sit on. Would you like to try and grab the branch or try and climb the rock? grab/climb ")
         if answer.lower() == "grab":
             print(
                 "The branch breaks and you fall down the waterfall. You lose. Try Again!")
@@ -22,7 +22,7 @@ while True:
             continue
     elif answer.lower() == "walk":
         answer = input(
-            "You make it safely across the brige. After some time you come across a cave. You go inside, and see a diamond sitting on the floor. Do you try and grab the diamond or leave the cave? ")
+            "You make it safely across the brige. After some time you come across a cave. You go inside, and see a diamond sitting on the floor. Do you try and grab the diamond or leave the cave? grab/leave ")
         if answer.lower() == "grab":
             print(
                 "You grab the diamond and the floor beneath you opens to reveal a hole. You lose. Try Again!")
